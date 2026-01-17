920c7c7d80ff09e17723938a9a440ea23a1a34a4
Ignacio <185011694+nimbusphagia@users.noreply.github.com>
2026-01-16 20:47:23 -0500

    refactor indexGet to a join

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,12 +2,30 @@ import { poolQuery } from "../database/index.js";
 
 async function indexGet(req, res) {
   // Get all books
-  const sql = 'SELECT * FROM book';
-  const result = await poolQuery(sql);
-  const books = result.rows;
-  res.render('index', {
-    books: books,
-  })
+  const sql = `
+    SELECT 
+      b.title,
+      a.first_name || ' ' || a.last_name as author,
+      p.publisher_name as publisher,
+      b.year,
+      b.description,
+      b.isbn
+    FROM book b
+    JOIN author a 
+    ON b.author_id = a.id 
+    JOIN publisher p 
+    ON p.id = b.publisher_id
+  `;
+  try {
+    const result = await poolQuery(sql);
+    const books = result.rows;
+    res.render('index', {
+      books: books,
+    })
+
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export { indexGet };
